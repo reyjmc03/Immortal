5e6f170b2c5fc46b8e80a27fe6c15242bfa1ce6d
minhld_570 <minhle2994@gmail.com>
2014-04-28 11:35:10 +0700

    create buy button

--- Assets/Script/BodyControl.cs
+++ Assets/Script/BodyControl.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class BodyControl : MonoBehaviour {
 	public GameObject body_tower;
 	public GameObject top_tower;
+	public GameObject button;
 	// Use this for initialization
 	void Start () {
 		Instantiate (body_tower, new Vector3 (7.69f, -30f, 0), body_tower.transform.rotation);
