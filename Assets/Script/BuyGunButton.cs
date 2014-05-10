f5f502e74ec64007e4eafdb78614897bf139d39b
minhld_570 <minhle2994@gmail.com>
2014-05-10 11:03:01 +0700

    buy heavygun

--- Assets/Script/BuyGunButton.cs
+++ Assets/Script/BuyGunButton.cs
@@ -7,6 +7,9 @@ public class BuyGunButton : MonoBehaviour
     public bool canClick = true;
     public GameObject bodyControl;
     public GameObject buyButtonControl;
+	public GameObject heavyGun1Prefab;
+	public GameObject heavyGun1;
+	public GameObject parent;
 
     void Start()
     {
@@ -27,5 +30,9 @@ public class BuyGunButton : MonoBehaviour
     {
         bodyControl.GetComponent<BodyControl>().number_Of_Floor++;
         buyButtonControl.GetComponent<BuyButtonControl>().closeButton();
-    }
+		heavyGun1 = Instantiate(heavyGun1Prefab) as GameObject;
+		heavyGun1.transform.parent = parent.transform;
+		heavyGun1.transform.localPosition = new Vector3(-4, -300, 0);
+		heavyGun1.transform.localScale = new Vector3(1, 1, 1);
+	}
 }
--- Assets/Script/Constants.cs
+++ Assets/Script/Constants.cs
@@ -9,5 +9,6 @@ public static class Constants
 	public static Vector3 BUY_LIGHTNING_BUTTON_END_POSITION = new Vector3 (280, -270, 0);
 	public static Vector3 BUY_MACHINE_GUN_BUTTON_END_POSITION = new Vector3 (-280, -270, 0);
 	public static Vector3 BUY_SAW_BUTTON_END_POSITION = new Vector3 (-145, -250, 0);
+	public static Vector3 GUN_BASE_POSITION = new Vector3(-4, -300, 0);
 }
 
